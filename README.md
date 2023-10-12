2860c2cb031fc113cb7fb031c9597d5d5e6a5f0d
Tudor Jianu <jianu.tudor9@gmail.com>
2023-10-12 08:42:21 +0100

    add buttons

--- README.md
+++ README.md
@@ -7,5 +7,3 @@
 - [ ] Add a div for displaying results and change all of your console.logs into DOM methods.
 - [ ] Display the running score, and announce a winner of the game once one player reaches 5 points.
 - [ ] You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
-
-
--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,79 +1,70 @@
-const CHOICES = {
-  0: "draw",
-  1: "won",
-  2: "lost",
-};
+const totalRounds = 5;
+
+const scoreElement = document.getElementById("score");
+const roundElement = document.getElementById("round");
+
+let currentRound = parseInt(roundElement.textContent);
+let currentScore = parseInt(scoreElement.textContent);
 
 function getComputerChoice() {
-  randomNumber = Math.floor(Math.random() * 3)
-  switch (randomNumber) {
-    case 0:
-      return "rock";
-    case 1:
-      return "paper";
-    case 2:
-      return "scissors";
-    default:
-      break;
-  }
+  const choices = ["rock", "paper", "scissors"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
 }
 
+function determineWinner(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return 0; // draw
+  }
+
+  const winningConditions = {
+    rock: 'scissors',
+    paper: 'rock',
+    scissors: 'paper'
+  };
+
+  return winningConditions[playerSelection] === computerSelection ? 1 : 0;
+}
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase()
-  switch (playerSelection) {
-    case "rock":
-      switch (computerSelection) {
-        case "rock":
-          return 0;
-        case "paper":
-          return 2;
-        case "scissors":
-          return 1;
-      }
-    case "paper":
-      switch (computerSelection) {
-        case "rock":
-          return 1;
-        case "paper":
-          return 0;
-        case "scissors":
-          return 2;
-      }
-    case "scissors":
-      switch (computerSelection) {
-        case "rock":
-          return 2;
-        case "paper":
-          return 1;
-        case "scissors":
-          return 0;
-      }
-    default:
-      throw "Invalid choice: You can only choose Rock/Paper/Scissors"
+  playerSelection = playerSelection.toLowerCase();
+
+  currentScore += determineWinner(playerSelection, computerSelection);
+  currentRound++;
+
+  scoreElement.textContent = currentScore;
+  roundElement.textContent = currentScore.toString().padStart(2, "0");
+
+  if (currentRound === totalRounds) {
+    displayGameResult();
+    resetGame();
   }
 }
 
-let score = 0;
-for (i = 0; i < 5; i++) {
-  let playerSelection = prompt("Choice? (Rock/Paper/Scissors): ");
-  let computerSelection = getComputerChoice()
-  let result = playRound(playerSelection, computerSelection)
-  if (result === 1) {
-    score += 1
+function displayGameResult() {
+  const middle = totalRounds / 2;
+
+  if (currentScore > middle) {
+    console.log("You Won!");
+  } else if (currentScore < middle) {
+    console.log("You Lost!");
   } else {
-    score -= 1
+    console.log("Draw!");
   }
-  console.log(`Round ${i}: You ${CHOICES[result]}!`)
-}
-if (score > 0) {
-  console.log(`You won!`)
-} else if (score < 0) {
-  console.log("You lost!")
-} else {
-  console.log("draw!")
 }
 
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
+function resetGame() {
+  currentRound = 0;
+  currentScore = 0;
+
+  roundElement.textContent = currentScore.toString().padStart(2, "0");
+  scoreElement.textContent = currentScore;
+}
+
+const choices = ['rock', 'paper', 'scissors'];
+choices.forEach(choice => {
+  const button = document.getElementById(choice);
+  button.addEventListener('click', () => {
+    playRound(choice, getComputerChoice());
+  });
+});
